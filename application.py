4745a29ffd2f8a0b9008cdd97d0b2281b207a9c9
Sourish Dutta <sourish.d@gmail.com>
2020-05-13 22:25:38 -0500

    bootstrap buttons added to index and login pages

--- application.py
+++ application.py
@@ -6,9 +6,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
+#DATABASE_URL = 'postgresql+psycopg2://qlesouarqdzvpk:667a3563007212e9a1d9b5b478fa439f17a5e0e32d2eff602855fd0d399b3fb3@ec2-18-209-187-54.compute-1.amazonaws.com:5432/d10dsm9g49lrku';
+DATABASE_URL = 'sqlite:///books.db'
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
+if not os.getenv(DATABASE_URL):
     raise RuntimeError("DATABASE_URL is not set")
 
 # Configure session to use filesystem
@@ -17,7 +19,7 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv(DATABASE_URL))
 db = scoped_session(sessionmaker(bind=engine))
 
 
